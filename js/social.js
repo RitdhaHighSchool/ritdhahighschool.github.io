6fe8d97b1f3637f3af26f1b83735681d70268ee1
RitdhaHighSchool <gemannihilator8@gmail.com>
2021-08-15 21:41:55 +0700

    Update

--- js/social.js
+++ js/social.js
@@ -1,25 +1,31 @@
 jQuery(document).ready(function () {
 	if (/Android/i.test(navigator.userAgent)) {
-    $('.twitter').attr('href', 'intent://twitter.com/TheGiftedGlobal#Intent;package=com.twitter.android;scheme=https;end');
+		$('.twitter').attr('href', 'intent://twitter.com/TheGiftedGlobal#Intent;package=com.twitter.android;scheme=https;end');
 		$('.fbpage').attr('href', 'intent://profile/492376137854324#Intent;package=com.facebook.katana;scheme=fb;S.browser_fallback_url=https://facebook.com/TheGiftedGlobal;end');
 		$('.instagram').attr('href', 'intent://www.instagram.com/thegiftedglobal/#Intent;package=com.instagram.android;scheme=https;end');
 	}
 
 	if (/iPhone|iPad|iPod|^i/i.test(navigator.userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) {
 		var theme = document.querySelector('#theme');
-		var _overlay = $('#themeoverlay');
+		var _overlay = document.querySelector('#themeoverlay');
 
-		$(document).on('click touchstart tap', '#themeoverlay', function (e) {
-			e.preventDefault();
-			theme.play();
-			_overlay.remove();
-		}, false);			
+		['click', 'touchstart'].forEach(evt =>
+			_overlay.addEventListener(evt, function (e) {
+				theme.play();
+				_overlay.remove();
+			}, false)
+		);
 
 		setTimeout(function () {
-			_overlay.css('display', 'block')
+			_overlay.style.display = 'block';
 		}, 1000);
 
-    $('.twitter').click(function (e) {
+		document.addEventListener('touchmove', function (e) {
+			e.preventDefault();
+			//  _overlay.style.display = 'block';
+		}, false);
+
+		$('.twitter').click(function (e) {
 			e.stopPropagation();
 			e.preventDefault();
 			window.location = 'twitter://user?screen_name=TheGiftedGlobal';
@@ -27,14 +33,14 @@ jQuery(document).ready(function () {
 				window.location = 'https://twitter.com/TheGiftedGlobal'
 			}, 250);
 		});
-    $('.fbpage').click(function (e) {
+		$('.fbpage').click(function (e) {
 			e.stopPropagation();
 			e.preventDefault();
 			window.location = 'fb://page/?id=492376137854324';
 			setTimeout(function () {
 				window.location = 'https://facebook.com/TheGiftedGlobal/'
 			}, 250);
-		});		
+		});
 		$('.instagram').click(function (e) {
 			e.stopPropagation();
 			e.preventDefault();
