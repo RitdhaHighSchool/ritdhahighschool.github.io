868a7144223bd77e7bb9b3e1806f919e149227c7
RitdhaHighSchool <gemannihilator8@gmail.com>
2021-08-15 20:26:18 +0700

    Update

---
+++ js/social.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function () {
+	if (/Android/i.test(navigator.userAgent)) {
+    $('.twitter').attr('href', 'intent://twitter.com/TheGiftedGlobal#Intent;package=com.twitter.android;scheme=https;end');
+		$('.fbpage').attr('href', 'intent://profile/492376137854324#Intent;package=com.facebook.katana;scheme=fb;S.browser_fallback_url=https://facebook.com/TheGiftedGlobal;end');
+		$('.instagram').attr('href', 'intent://www.instagram.com/thegiftedglobal/#Intent;package=com.instagram.android;scheme=https;end');
+	}
+
+	if (/iPhone|iPad|iPod|^i/i.test(navigator.userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) {
+    $('.twitter').click(function (e) {
+			e.stopPropagation();
+			e.preventDefault();
+			window.location = 'twitter://user?screen_name=TheGiftedGlobal';
+			setTimeout(function () {
+				window.location = 'https://twitter.com/TheGiftedGlobal'
+			}, 250);
+		});
+    $('.fbpage').click(function (e) {
+			e.stopPropagation();
+			e.preventDefault();
+			window.location = 'fb://page/?id=492376137854324';
+			setTimeout(function () {
+				window.location = 'https://facebook.com/TheGiftedGlobal/'
+			}, 250);
+		});		
+		$('.instagram').click(function (e) {
+			e.stopPropagation();
+			e.preventDefault();
+			window.location = 'instagram://user?username=thegiftedglobal';
+			setTimeout(function () {
+				window.location = 'https://www.instagram.com/thegiftedglobal/'
+			}, 250);
+		});
+	}
+});
